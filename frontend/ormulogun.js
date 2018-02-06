9678ad5bab93fee4ce5d63d4c3491e7ba565f8b3
Artefact2 <artefact2@gmail.com>
2018-02-06 10:07:45 +0100

    Use let instead of var in ormulogun.js

--- frontend/ormulogun.js
+++ frontend/ormulogun.js
@@ -20,29 +20,29 @@ const GUMBLE_MOVE_SIZE = 4; /* == sizeof(cch_move_t) */
 const GUMBLE_SAFE_FEN_LENGTH = 90; /* >= SAFE_FEN_LENGTH */
 const GUMBLE_SAFE_ALG_LENGTH = 10; /* >= SAFE_ALG_LENGTH */
 
-var gumble_board = null;
-var gumble_fen_str = null;
-var gumble_move_str = null;
-var gumble_move = null;
+let gumble_board = null;
+let gumble_fen_str = null;
+let gumble_move_str = null;
+let gumble_move = null;
 
-var orm_manifest = null;
-var orm_puzzle_set = null;
-var orm_puzzle_midx = null;
-var orm_puzzle_idx = null;
-var orm_puzzle = null;
-var orm_puzzle_next = null;
-var orm_movelist = null;
-var orm_movelist_idx = null;
-var orm_candidate_move = null;
+let orm_manifest = null;
+let orm_puzzle_set = null;
+let orm_puzzle_midx = null;
+let orm_puzzle_idx = null;
+let orm_puzzle = null;
+let orm_puzzle_next = null;
+let orm_movelist = null;
+let orm_movelist_idx = null;
+let orm_candidate_move = null;
 
-var orm_error = function(str) {
-	var err = $(document.createElement('p'));
+let orm_error = function(str) {
+	let err = $(document.createElement('p'));
 	err.addClass('alert alert-danger');
 	err.text(str);
 	$("nav#mainnav").after(err);
 };
 
-var orm_load_puzzle_manifest = function(done) {
+let orm_load_puzzle_manifest = function(done) {
 	$.getJSON("./puzzles/manifest.js").always(function() {
 		$("div#select-puzzleset > p.alert").remove();
 	}).fail(function() {
@@ -50,24 +50,24 @@ var orm_load_puzzle_manifest = function(done) {
 	}).done(function(data) {
 		orm_manifest = data;
 
-		var ul = $("div#select-puzzleset > ul");
-		var hash = location.hash.split('-', 3);
-		for(var i in data) {
-			var pset = data[i];
-			var li = $(document.createElement('li'));
+		let ul = $("div#select-puzzleset > ul");
+		let hash = location.hash.split('-', 3);
+		for(let i in data) {
+			let pset = data[i];
+			let li = $(document.createElement('li'));
 			li.addClass('row');
 
-			var h = $(document.createElement('h3'));
+			let h = $(document.createElement('h3'));
 			h.text(pset.name);
 			h.addClass('col');
 			li.append(h);
 
-			var p = $(document.createElement('p'));
+			let p = $(document.createElement('p'));
 			p.text(pset.desc + " " + pset.count + " puzzles.");
 			p.addClass('col-6');
 			li.append(p);
 
-			var btn = $(document.createElement('button'));
+			let btn = $(document.createElement('button'));
 			btn.text('Start training');
 			btn.addClass('btn btn-primary col start-training');
 			btn.data('idx', i);
@@ -77,7 +77,7 @@ var orm_load_puzzle_manifest = function(done) {
 		}
 
 		ul.find("button.start-training").click(function() {
-			var t = $(this);
+			let t = $(this);
 			t.prop('disabled', 'disabled');
 			t.addClass('disabled');
 			orm_load_puzzle_set(t.data('idx'), null, null, function() {
@@ -92,8 +92,8 @@ var orm_load_puzzle_manifest = function(done) {
 	});
 };
 
-var orm_load_puzzle_set = function(m_idx, always, fail, done) {
-	var p = $(document.createElement('p'));
+let orm_load_puzzle_set = function(m_idx, always, fail, done) {
+	let p = $(document.createElement('p'));
 	p.addClass("alert alert-primary").text("Loading the puzzle set…");
 	$("nav#mainnav").after(p);
 
@@ -110,9 +110,9 @@ var orm_load_puzzle_set = function(m_idx, always, fail, done) {
 	});
 };
 
-var orm_restore_tab = function() {
+let orm_restore_tab = function() {
 	$("div#intro").hide();
-	var h = location.hash.split("-", 3);
+	let h = location.hash.split("-", 3);
 	switch(h[0]) {
 	case "#list":
 		$("div#select-puzzleset").show();
@@ -121,8 +121,8 @@ var orm_restore_tab = function() {
 	case "#puzzle":
 		/* XXX */
 		if(h.length !== 3) break;
-		var manifest_idx = null;
-		for(var i in orm_manifest) {
+		let manifest_idx = null;
+		for(let i in orm_manifest) {
 			if(orm_manifest[i].id === h[1]) {
 				manifest_idx = i;
 				break;
@@ -132,7 +132,7 @@ var orm_restore_tab = function() {
 		orm_load_puzzle_set(manifest_idx, null, function() {
 			$("div#intro").show();
 		}, function() {
-			var idx = parseInt(h[2]);
+			let idx = parseInt(h[2]);
 			orm_load_puzzle(idx);
 			$("div#play-puzzle").show();
 		});
@@ -142,11 +142,11 @@ var orm_restore_tab = function() {
 	$("div#intro").show();
 };
 
-var orm_init_board = function() {
-	var b = $("div#board");
-	var d;
-	for(var f = 1; f <= 8; ++f) {
-		for(var r = 1; r <= 8; ++r) {
+let orm_init_board = function() {
+	let b = $("div#board");
+	let d;
+	for(let f = 1; f <= 8; ++f) {
+		for(let r = 1; r <= 8; ++r) {
 			d = $(document.createElement('div'));
 			d.addClass("back r" + r + " f" + f + " " + ((f + r) % 2 ? 'light' : 'dark'));
 			d.data("ofile", f);
@@ -156,12 +156,12 @@ var orm_init_board = function() {
 	}
 };
 
-var orm_load_fen = function(fen) {
-	var b = $("div#board");
-	var p, r = 8, f = 1, cl;
+let orm_load_fen = function(fen) {
+	let b = $("div#board");
+	let p, r = 8, f = 1, cl;
 	b.children("div.piece").remove();
 
-	for(var i = 0; i < fen.length && fen[i] != " "; ++i) {
+	for(let i = 0; i < fen.length && fen[i] != " "; ++i) {
 		switch(fen[i]) {
 		case '/':
 			f = 1;
@@ -209,7 +209,7 @@ var orm_load_fen = function(fen) {
 		++f;
 	}
 
-	var side = fen.split(" ", 3)[1];
+	let side = fen.split(" ", 3)[1];
 	if(side === "w") {
 		b.removeClass("black").addClass("white");
 	} else {
@@ -219,18 +219,18 @@ var orm_load_fen = function(fen) {
 	b.data('fen', fen);
 }
 
-var orm_animate_move = function(startfen, movelan, endfen, start, done, delay) {
+let orm_animate_move = function(startfen, movelan, endfen, start, done, delay) {
 	if(startfen) orm_load_fen(startfen);
 
-	var sf, sr, ef, er, cl;
+	let sf, sr, ef, er, cl;
 	sf = movelan.charCodeAt(0) - "a".charCodeAt(0) + 1;
 	sr = movelan.charCodeAt(1) - "1".charCodeAt(0) + 1;
 	ef = movelan.charCodeAt(2) - "a".charCodeAt(0) + 1;
 	er = movelan.charCodeAt(3) - "1".charCodeAt(0) + 1;
 	/* XXX: promotion */
 
-	var psrc = $("div#board > div.piece.f" + sf + ".r" + sr);
-	var pdest = $("div#board > div.piece.f" + ef + ".r" + er);
+	let psrc = $("div#board > div.piece.f" + sf + ".r" + sr);
+	let pdest = $("div#board > div.piece.f" + ef + ".r" + er);
 
 	setTimeout(function() {
 		psrc.removeClass("f" + sf + " r" + sr);
@@ -247,7 +247,7 @@ var orm_animate_move = function(startfen, movelan, endfen, start, done, delay) {
 	}, typeof(delay) === "undefined" ? 750 : delay);
 };
 
-var orm_push_move = function(startfen, movesan, movelan, endfen) {
+let orm_push_move = function(startfen, movesan, movelan, endfen) {
 	if(orm_movelist !== null && orm_movelist_idx !== orm_movelist.length - 1) {
 		orm_error("Can't push a move in this position.");
 		return;
@@ -263,30 +263,30 @@ var orm_push_move = function(startfen, movesan, movelan, endfen) {
 
 	$("table#movelist button.btn-primary").removeClass("btn-primary").addClass("btn-light");
 
-	var btn = $(document.createElement('button'));
+	let btn = $(document.createElement('button'));
 	btn.text(movesan);
 	btn.data('idx', orm_movelist_idx);
 	btn.attr("id", "ply" + orm_movelist_idx);
 	btn.addClass('btn btn-sm btn-block btn-primary');
 
-	var tb = $("table#movelist > tbody");
-	var tr = tb.children('tr').last();
-	var td;
+	let tb = $("table#movelist > tbody");
+	let tr = tb.children('tr').last();
+	let td;
 	if(tr.length === 0 || tr.find('button').length === 2) {
 		tr = $(document.createElement("tr"));
 		tb.append(tr);
 
-		var th = $(document.createElement("th"));
+		let th = $(document.createElement("th"));
 		th.text(Math.floor((orm_puzzle.ply + orm_movelist.length) / 2) + ".");
 		tr.append(th);
 
 		td = $(document.createElement("td"));
 		tr.append(td);
-		var td2 = $(document.createElement("td"));
+		let td2 = $(document.createElement("td"));
 		tr.append(td2);
 
 		if((orm_puzzle.ply  + orm_movelist.length) % 2) {
-			var dummy = $(document.createElement("button"));
+			let dummy = $(document.createElement("button"));
 			dummy.text("...");
 			dummy.addClass("btn btn-sm btn-block btn-light disabled");
 			dummy.prop("disabled", "disabled");
@@ -300,10 +300,10 @@ var orm_push_move = function(startfen, movesan, movelan, endfen) {
 	td.append(btn);
 };
 
-var orm_load_puzzle = function(idx) {
+let orm_load_puzzle = function(idx) {
 	orm_puzzle_idx = idx;
 	orm_movelist = orm_movelist_idx = null;
-	var puz = orm_puzzle = orm_puzzle_set[idx];
+	let puz = orm_puzzle = orm_puzzle_set[idx];
 	$("table#movelist > tbody").empty();
 	$("a#puzzle-analysis").prop('href', 'https://lichess.org/analysis/' + orm_puzzle.reply.fen.replace(/\s/g, '_'));
 	history.replaceState(null, null, "#puzzle-" + orm_manifest[orm_puzzle_midx].id + "-" + idx);
@@ -323,25 +323,25 @@ var orm_load_puzzle = function(idx) {
 	orm_puzzle_next = puz.next;
 };
 
-var orm_puzzle_over = function() {
+let orm_puzzle_over = function() {
 	$("div#puzzle-actions-after").addClass('visible');
 	$("button#puzzle-abandon").hide();
 	$("button#puzzle-next").show();
 	orm_puzzle_next = null;
 };
 
-var orm_puzzle_success = function() {
+let orm_puzzle_success = function() {
 	$("p#puzzle-prompt").addClass("text-success").text("Puzzle completed successfully!");
 	$("nav#mainnav").addClass("bg-success");
 	orm_puzzle_over();
 };
 
-var orm_puzzle_fail = function() {
-	var oldidx = orm_movelist_idx;
-	var oldbtn = $("table#movelist button.btn-primary");
+let orm_puzzle_fail = function() {
+	let oldidx = orm_movelist_idx;
+	let oldbtn = $("table#movelist button.btn-primary");
 	while(orm_puzzle_next !== null) {
-		for(var lan in orm_puzzle_next) {
-			var puz = orm_puzzle_next[lan];
+		for(let lan in orm_puzzle_next) {
+			let puz = orm_puzzle_next[lan];
 			orm_push_move(orm_movelist[orm_movelist_idx][3], puz.move.san, lan, puz.move.fen); /* XXX */
 			orm_push_move(puz.move.fen, puz.reply.san, puz.reply.lan, puz.reply.fen);
 			orm_puzzle_next = puz.next;
@@ -358,18 +358,18 @@ var orm_puzzle_fail = function() {
 	orm_puzzle_over();
 };
 
-var orm_can_move_piece = function(p) {
-	var b = $("div#board");
+let orm_can_move_piece = function(p) {
+	let b = $("div#board");
 	if(p.hasClass("white") !== b.hasClass("white") || p.hasClass("black") !== b.hasClass("black")) return false;
 	if(orm_puzzle !== null && orm_puzzle_next !== null && p.hasClass("black") !== !!(orm_puzzle.ply % 2)) return false;
 	if(orm_puzzle_next !== null && orm_movelist_idx !== orm_movelist.length - 1) return false;
 	return true;
 };
 
-var orm_do_user_move = function(lan, animate) {
+let orm_do_user_move = function(lan, animate) {
 	/* XXX: promote */
 
-	var sf, sr, tf, tr;
+	let sf, sr, tf, tr;
 	sf = lan.charCodeAt(0) - "a".charCodeAt(0) + 1;
 	sr = lan.charCodeAt(1) - "1".charCodeAt(0) + 1;
 	tf = lan.charCodeAt(2) - "a".charCodeAt(0) + 1;
@@ -378,7 +378,7 @@ var orm_do_user_move = function(lan, animate) {
 	if(sf < 1 || sf > 8 || sr < 1 || sr > 8 || tf < 1 || tf > 8 || tr < 1 || tr > 8) return false;
 	if(sf === tf && sr === tr) return false;
 
-	var b = $("div#board");
+	let b = $("div#board");
 
 	writeAsciiToMemory(b.data('fen'), gumble_fen_str);
 	writeAsciiToMemory(lan, gumble_move_str);
@@ -389,7 +389,7 @@ var orm_do_user_move = function(lan, animate) {
 	}
 	b.toggleClass("white black");
 
-	var after = function() {
+	let after = function() {
 		if(orm_puzzle_next === null) {
 			Module._cch_play_legal_move(gumble_board, gumble_move, 0);
 			Module._cch_save_fen(gumble_board, gumble_fen_str, GUMBLE_SAFE_FEN_LENGTH);
@@ -399,7 +399,7 @@ var orm_do_user_move = function(lan, animate) {
 		}
 
 		if(lan in orm_puzzle_next) {
-			var puz = orm_puzzle_next[lan];
+			let puz = orm_puzzle_next[lan];
 			orm_animate_move(puz.move.fen, puz.reply.lan, puz.reply.fen, function() {
 				orm_push_move(puz.move.fen, puz.reply.san, puz.reply.lan, puz.reply.fen);
 			});
@@ -420,7 +420,7 @@ var orm_do_user_move = function(lan, animate) {
 	} else {
 		b.children("div.piece.r" + tr + ".f" + tf).remove();
 
-		var p = b.children("div.piece.r" + sr + ".f" + sf);
+		let p = b.children("div.piece.r" + sr + ".f" + sf);
 		p.removeClass("r" + sr + " f" + sf);
 		p.addClass("r" + tr + " f" + tf);
 		p.data("orank", tr).data("ofile", tf);
@@ -465,9 +465,9 @@ $(function() {
 	});
 
 	$("table#movelist").on("click", "button", function() {
-		var b = $(this);
+		let b = $(this);
 		orm_movelist_idx = b.data("idx");
-		var m = orm_movelist[orm_movelist_idx];
+		let m = orm_movelist[orm_movelist_idx];
 		$("table#movelist button.btn-primary").removeClass("btn-primary").addClass("btn-light");
 		b.removeClass("btn-light").addClass("btn-primary");
 		orm_animate_move(m[0], m[2], m[3], null, null, 100);
@@ -476,10 +476,10 @@ $(function() {
 	/* XXX not touch-friendly */
 	/* XXX highlight possible targets using gumble movegen */
 	$("div#board").on("mousedown", "> div.piece", function(e) {
-		var p = $(this);
+		let p = $(this);
 		if(!orm_can_move_piece(p)) return;
 
-		var bg = $("div#board > div.back.r" + p.data("orank") + ".f" + p.data("ofile"));
+		let bg = $("div#board > div.back.r" + p.data("orank") + ".f" + p.data("ofile"));
 		bg.addClass("drag-source");
 		p.data("origx", e.pageX);
 		p.data("origy", e.pageY);
@@ -487,17 +487,17 @@ $(function() {
 		p.data("origleft", p.position().left);
 		p.addClass("dragging");
 	}).on("mousemove", function(e) {
-		var p = $("div#board > div.piece.dragging");
+		let p = $("div#board > div.piece.dragging");
 		if(!p) return;
 
 		p.addClass("dragged");
 		p.css("left", p.data("origleft") + e.pageX - p.data("origx"));
 		p.css("top", p.data("origtop") + e.pageY - p.data("origy"));
 	}).on("mouseup", "> div.piece.dragging", function(e) {
-		var p = $(this);
-		var b = $("div#board"), pos = p.position();
-		var file = 1 + Math.round(8 * pos.left / b.width());
-		var rank = 9 - Math.round(1 + 8 * pos.top / b.height());
+		let p = $(this);
+		let b = $("div#board"), pos = p.position();
+		let file = 1 + Math.round(8 * pos.left / b.width());
+		let rank = 9 - Math.round(1 + 8 * pos.top / b.height());
 
 		if($("div#board").hasClass('flipped')) {
 			file = 9 - file;
@@ -516,7 +516,7 @@ $(function() {
 		p.removeClass('dragging');
 		$("div#board > div.drag-source").removeClass("drag-source");
 	}).on("click", "> div", function() {
-		var p = $(this);
+		let p = $(this);
 		if(p.hasClass("dragged")) {
 			orm_candidate_move = null;
 			p.removeClass("dragged");
@@ -525,7 +525,7 @@ $(function() {
 		if(orm_candidate_move === null && (p.hasClass("back") || (p.hasClass("piece") && !orm_can_move_piece(p)))) return;
 
 		if(orm_candidate_move === null) {
-			var bg = $("div#board > div.back.r" + p.data("orank") + ".f" + p.data("ofile"));
+			let bg = $("div#board > div.back.r" + p.data("orank") + ".f" + p.data("ofile"));
 			bg.addClass("drag-source");
 			orm_candidate_move = String.fromCharCode(
 				"a".charCodeAt(0) + p.data("ofile") - 1,
@@ -534,7 +534,7 @@ $(function() {
 		} else {
 			$("div#board > div.drag-source").removeClass("drag-source");
 
-			var tgt = String.fromCharCode(
+			let tgt = String.fromCharCode(
 				"a".charCodeAt(0) + p.data("ofile") - 1,
 				"1".charCodeAt(0) + p.data("orank") - 1,
 			);
